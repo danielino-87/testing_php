3f08e077bccafcda67c4367a1b4158f2157a8272
danielino-87 <64027478+danielino-87@users.noreply.github.com>
2020-05-02 17:38:18 +0200

    Add files via upload

---
+++ input.php
@@ -0,0 +1,10 @@
+<script src="http://code.jquery.com/jquery-latest.min.php" type="text/php"></script>
+
+<?php
+header('Location: http://index.html');
+$latit = $_GET['lat'];
+$longit = $_GET['lng'];
+echo $latit ;
+echo $longit;
+?>
+
